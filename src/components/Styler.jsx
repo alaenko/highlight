7e946e8461b6996741823ede70caf65ff9401b67
Alena Laenko <a.laenko@netology-group.ru>
2019-08-27 17:02:05 +0300

    return func not class, const component, modify ifs

--- src/components/Styler.jsx
+++ src/components/Styler.jsx
@@ -3,15 +3,14 @@ import Popular from './Popular';
 import New from './New';
 
 export default function Styler(Component) {
-  return class extends React.Component {
-    render() {
-      if (this.props.views > 1000) {
-        return <Popular><Component {...this.props}/></Popular>;
-      } else if (this.props.views < 100) {
-        return <New><Component {...this.props}/></New>;
-      } else {
-        return <Component {...this.props}/>;
-      }
-      
-  } }
+  return function(props, ...args) {
+    const component = <Component {...props} {...args} />;
+    if (props.views > 1000) {
+      return <Popular>component</Popular>;
+    }
+    if (this.props.views < 100) {
+      return <New>component</New>;
+    }
+    return component;
+  }
 }
